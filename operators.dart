18e597f4b1823596c6c14b4fae68991dd5461f9a
HassanCodeWave <shforshaikhhassan@gmail.com>
2024-12-04 01:10:04 +0500

    combine file added and notes added in operators.dart

---
+++ operators.dart
@@ -0,0 +1,14 @@
+// Operators In Dart
+// Operators are used to perform mathematical and logical operations on the variables. Each operation in dart uses a symbol called the operator to denote the type of operation it performs. Before learning operators in the dart, you must understand the following things.
+
+// Operands : It represents the data.
+// Operator : It represents how the operands will be processed to produce a value.
+
+// Types Of Operators
+// There are different types of operators in dart. They are as follows:
+
+// Arithmetic Operators
+// Increment and Decrement Operators
+// Assignment Operators
+// Logical Operators
+// Type Test Operators
